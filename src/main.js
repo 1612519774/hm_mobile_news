393918f8ded98a63fcfdad5e15ff74f7f1b695f4
1612519774 <1612519774@qq.com>
2019-07-29 17:40:51 +0800

    登录-配置Vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from '@/store'
 import 'amfe-flexible/index.min.js'
 import Vant from 'vant'
 import 'vant/lib/index.css'
-Vue.use(Vuex)
 Vue.use(Vant)
 
 Vue.config.productionTip = false
